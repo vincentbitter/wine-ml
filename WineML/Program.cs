677716da1337efdfb8955824e097b7facd5f36a2
Vincent Bitter <v.bitter@maxiblue.nl>
2019-08-01 21:17:32 +0200

    Load data from CSV files

--- WineML/Program.cs
+++ WineML/Program.cs
@@ -1,3 +1,4 @@
+﻿using Microsoft.ML;
 using System;
 using System.IO;
 
@@ -5,12 +6,42 @@ namespace WineML
 {
     class Program
     {
+        private static string[] _features = new[] {
+            nameof(WineData.FixedAcidity),
+            nameof(WineData.VolatileAcidity),
+            nameof(WineData.CitricAcid),
+            nameof(WineData.ResidualSugar),
+            nameof(WineData.Chlorides),
+            nameof(WineData.FreeSulfurDioxide),
+            nameof(WineData.TotalSulfurDioxide),
+            nameof(WineData.Density),
+            nameof(WineData.Ph),
+            nameof(WineData.Sulphates),
+            nameof(WineData.Alcohol)
+        };
+
         static void Main(string[] args)
         {
             PrintHeader("Tasting wine with ML.NET");
 
+            var mlContext = new MLContext();
+
+            Console.Write("Load training data...");
+            var trainingData = LoadData(mlContext, "winequality-white-train.csv");
+            Console.WriteLine("DONE!");
+
+            Console.Write("Load validation data...");
+            var validationData = LoadData(mlContext, "winequality-white-validate.csv");
+            Console.WriteLine("DONE!");
+
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
+        }
+
+        private static IDataView LoadData(MLContext mlContext, string fileName)
+        {
+            var dataPath = Path.Combine(Environment.CurrentDirectory, "Data", fileName);
+            return mlContext.Data.LoadFromTextFile<WineData>(dataPath, separatorChar: ';', hasHeader: true);
         }
 
         private static void PrintHeader(string title)
