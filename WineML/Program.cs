b9f2710bd12ff942311fd68bb1bbd1260a0d93f5
Vincent Bitter <v.bitter@maxiblue.nl>
2019-08-01 19:52:10 +0200

    Add basic UI

--- WineML/Program.cs
+++ WineML/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.IO;
 
 namespace WineML
 {
@@ -6,6 +7,28 @@ namespace WineML
     {
         static void Main(string[] args)
         {
+            PrintHeader("Tasting wine with ML.NET");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        private static void PrintHeader(string title)
+        {
+            var originalForegroundColor = Console.ForegroundColor;
+            var originalBackgroundColor = Console.BackgroundColor;
+            
+            var padding = (Console.WindowWidth - title.Length) / 2;
+
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.BackgroundColor = ConsoleColor.White;
+
+            Console.WriteLine(".".PadLeft(Console.WindowWidth - 1, ' '));
+            Console.WriteLine(".".PadLeft(padding, ' ') + title.ToUpper() + "".PadLeft(padding - 1, ' '));
+            Console.WriteLine("".PadLeft(Console.WindowWidth - 1, ' '));
+
+            Console.ForegroundColor = originalForegroundColor;
+            Console.BackgroundColor = originalBackgroundColor;
         }
     }
 }
